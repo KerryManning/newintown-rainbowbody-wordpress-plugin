894d5d5b7ca561d3f14e1373b9c4734009347555
Kerry Manning <kerry@new-in-town.uk>
2017-08-31 14:13:26 +0100

    Fixed formatting and description

--- newintown-rainbowbody.php
+++ newintown-rainbowbody.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: NewInTown Rainbow Body
- * Description: Fed up of seeing the same old color background for the body of your site? Finding life boringly predictable? Get NewInTown Rainbow Body and you too could enjoy the excitement of a randomly generated background color every time you load a page!
+ * Description: Looking for a less predictable, more colorful lifestyle? Get NewInTown Rainbow Body and you too could enjoy the excitement of a randomly generated background color every time you load a page!
  * Version: 1.0
  * Author: Kerry Manning
  * Author URI: http://new-in-town.uk
@@ -23,17 +23,18 @@ class RainbowBody
 	}
 
 	function load_body_color () {
-			wp_enqueue_style( 'body-color', plugins_url( '/css/body-color.css', __FILE__ ) );
-			for (
-					$color = '', $i = 0, $max = strlen($hex_digit_options = '0123456789abcdef')-1;
-					$i != 6; 
-					$hex_digit = rand(0,$max), $color .= $hex_digit_options{$hex_digit}, $i++
-					);
-	 		$css = '.rainbow-body { background-color: #' . $color . '; }';
-	 		wp_add_inline_style( 'body-color', $css );
-		}
+		wp_enqueue_style( 'body-color', plugins_url( '/css/body-color.css', __FILE__ ) );
+		for (
+			$color = '', $i = 0, $max = strlen($hex_digit_options = '0123456789abcdef')-1;
+			$i != 6; 
+			$hex_digit = rand(0,$max), $color .= $hex_digit_options{$hex_digit}, $i++
+		);
+	 	$css = '.rainbow-body { background-color: #' . $color . '; }';
+	 	wp_add_inline_style( 'body-color', $css );
+	}
   
 }
+
 $make_body_colorful = new RainbowBody();
 
 
